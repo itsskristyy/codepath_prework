2e7665bdfede535bf64399bdb96cda8123043fe0
kristy delacruz <kristydelacruzx@gmail.com>
2022-07-15 14:10:12 -0400

    Added prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by kristy delacruz on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var Background: UIView!
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.white
+    }
+    
+    @IBAction func buttonBackhgroundColor(_ sender: Any) {
+        Background.backgroundColor = UIColor.systemPink
+    }
+    
+    @IBAction func buttonChangeText(_ sender: Any) {
+        TextLabel.text = "Goodbye"
+    }
+    
+    @IBAction func changeTextANYTHING(_ sender: Any) {
+        
+    }
+   
+    @IBAction func buttonRESET(_ sender: Any) {
+        TextLabel.textColor = UIColor.black
+        Background.backgroundColor = UIColor.systemBrown
+        TextLabel.text = "Hello"
+    }
+    
+    
+}
+
